02b17837332f7a917a152943b792719462d0bb20
Dagger7 <smol-d.a@mail.ru>
2022-06-03 16:45:34 +0500

    Changes backup / artifacts editor

---
+++ src/Components/AllArtifactsModal/ArtifactEditor/ArtifactSet.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Image} from "@chakra-ui/react";
+import styles from './../AllArtifactsModal.module.scss';
+
+const ArtifactSet = () => {
+  return (
+    <div className={styles.artifactSetContainer}>
+      <Image
+        className={styles.artifactSetImage}
+        src={`${process.env.PUBLIC_URL}/Assets/Artifacts/Adventurer/Flower.png`}
+        objectFit='cover'
+      />
+      <span>Adventurer</span>
+      {/*<span>props.setName</span>*/}
+    </div>
+  );
+};
+
+export default ArtifactSet;
